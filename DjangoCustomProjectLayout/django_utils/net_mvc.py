a5aea0e878d7c22787189411506fee482af2a35f
Jeysson Guevara <jeydotc@gmail.com>
2013-03-05 16:18:52 -0500

    Added Shared views folder lookup

--- DjangoCustomProjectLayout/django_utils/net_mvc.py
+++ DjangoCustomProjectLayout/django_utils/net_mvc.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.template.loader import select_template 
+from django.http import HttpResponse
 
 from convention import BootstrapConvention
 
@@ -18,6 +20,7 @@ class NetMvcConvention(BootstrapConvention):
     def run(self, request, app_name, controller_name, method_name = "index", *args, **kwargs):
         controller = self._get_module_by_name("%s.%s.controllers.%sController" % 
                                             (self._rootNamespace, app_name, controller_name))
+
         responder = getattr(controller, method_name, controller.index)
         responder.controller_name = controller_name
         responder.request = request
@@ -25,5 +28,7 @@ class NetMvcConvention(BootstrapConvention):
 
 def view(view_function, context = {}, template = None):
     t = view_function.func_name if template is None else template;
-    return render_to_response(view_function.controller_name + "/" + t + ".html", context,
-                          context_instance=RequestContext(view_function.request) )
+    return HttpResponse(select_template((
+                                "%s/%s.html" % (view_function.controller_name, t),
+                                "Shared/%s.html" % t,
+                            )).render(RequestContext(view_function.request, context)))
--- DjangoCustomProjectLayout/my_app/controllers/HelloWorldController.py
+++ DjangoCustomProjectLayout/my_app/controllers/HelloWorldController.py
@@ -10,3 +10,17 @@ def index(request):
 @require_GET
 def another_page(request):
     return view(another_page, { "content": "Foo content!" })
+
+def a_shared_view(request):
+    """
+    As this view is not at this' controller's views folder,
+    it'll be looked up at the Shared/ folder
+    """
+    return view(a_shared_view)
+
+def shared_view(request):
+    """
+    Same as above but as function's name doesn't coincide with
+    the template's name, we use the template= parameter. 
+    """
+    return view(shared_view, template="a_shared_view")
